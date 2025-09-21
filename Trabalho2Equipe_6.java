a383f722b9f9602580dff9d6fcc92150cbb53e3f
Luna Osti <luna.osti.etd@gmail.com>
2025-09-21 10:56:34 -0300

    Add files via upload

---
+++ Trabalho2Equipe_6.java
@@ -0,0 +1,17 @@
+
+package unespar.com.br.trabalho.equipe_6;
+
+import javax.swing.SwingUtilities;
+
+public class Trabalho2Equipe_6 {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(()-> {
+            InterfaceMenuPrincipal interfaceJogo = new InterfaceMenuPrincipal();
+        });
+               
+    }
+    
+            
+}
